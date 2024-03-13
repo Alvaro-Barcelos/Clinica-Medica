147d63c22632ce4f8ae0fa1b169160c539b4cd55
Rafael do Carmo Pereira <2005rafaeldocarmo2005@gmail.com>
2024-03-13 15:53:12 -0300

    comit 5

---
+++ cadastrar/cadastro.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Login</title>
+        
+    </head>
+    <body>
+	<?php
+
+	// REVISADO - OK
+	
+	include_once("conexao.php");
+
+		$nome = $_POST['nome'];
+		$cpf = $_POST['cpf'];
+		$data_nascimento = $_POST['data_nascimento'];
+		$sexo = $_POST['sexo'];
+		$email = $_POST['email'];
+		$telefone = $_POST['telefone'];  
+		$insere = mysqli_query ($conexao, “INSERT INTO Cliente (Nome_cliente, CPF, Telefone, Nascimento, Sexo, Email, Telefone)
+
+		VALUES ('$nome', '$cpf', '$data_nascimento', '$sexo', '$email', '$telefone')”)
+
+		or die (mysqli_error());
+		echo “Cliente inserido com sucesso!”; 
+		?>
+
+    </body> 
+</html>
