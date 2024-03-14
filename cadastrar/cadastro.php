b46525828aaf5a0d64fa34c5b941caa782954096
ana2602 <anarmsouza2602@gmail.com>
2024-03-14 16:49:51 -0300

    Update cadastro.php

--- cadastrar/cadastro.php
+++ cadastrar/cadastro.php
@@ -4,9 +4,44 @@
     <head>
         <meta charset="utf-8">
         <title>Login</title>
+		<link rel="stylesheet" href="home.css">
         
     </head>
     <body>
+
+	<header>
+            <div class="container">
+                
+                <p>Rua dos aimores, 469 - Centro - Belo Horizonte</p>
+                
+
+                <nav>
+                    <ul>
+                        <li><a href="">Telefone</a></li>
+                        <li><a href="#local">Mapa</a> </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+        <section>
+            <div>
+                <img src="../imagens/logo.png" alt="" height="50px" width="50px">
+                <h5>Clinica Ara</h5>
+                
+                
+                <nav>
+                    <ul>
+                        <li><a href="../home/home.html">Home</a></li>
+                        <li><a href="../home/home.html">Sobre nos</a></li>
+                        <li><a href="../agendar/agendar.html">Agendar</a></li>
+                        <li><a href="../cadastrar/cadastrar.html">Cadastrar</a></li>
+                        <li><a href="../consultas/consulta.html">Consultas</a></li>
+                    </ul>                    
+                </nav>
+                 
+            </div>
+        </section>
+
 	<?php
 
 	// REVISADO - OK
