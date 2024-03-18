5a753bf7fc1255ab012c0508787f5fca58680d99
Rafael do Carmo Pereira <2005rafaeldocarmo2005@gmail.com>
2024-03-18 16:54:02 -0300

    comit 10

--- cadastrar/cadastro.php
+++ cadastrar/cadastro.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 
 <html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>Login</title>
-		<link rel="stylesheet" href="home.css">
-        
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=, initial-scale=1.0">
+        <title>Document</title>
+            <link rel="stylesheet" href="style.css">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body>
 
@@ -58,8 +62,9 @@
 		$insere = mysqli_query ($conexao, "INSERT INTO Cliente (CPF, Nome_cliente, Nascimento, Sexo, Email, Telefone)
         VALUES ('$cpf', '$nome', '$data_nascimento', '$sexo', '$email', '$telefone')") or die (mysqli_error());
 
-		
-		echo "Cliente inserido com sucesso!"; 
+		echo "<div class='aviso'>";
+		echo "<p>Cliente inserido com sucesso!</p>"; 
+        echo "</div>"
 		?>
 
     </body> 
